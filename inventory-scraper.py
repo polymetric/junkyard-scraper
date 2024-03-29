7e82eea52366800fce889b142ac42e1941b3bada
Eli Rutan <erutan@freecast.com>
2024-03-29 16:44:04 -0400

    works

--- inventory-scraper.py
+++ inventory-scraper.py
@@ -29,9 +29,27 @@ chrome_options.add_argument('--disable-gpu')
 #specify the path to chromedriver.exe (download and save on your computer)
 driver = webdriver.Chrome(options=chrome_options)
 
+def scroll_down(f=None):
+    print('scrolling')
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    while True:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(3)
+        if f != None:
+            if f():
+                return
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+        print('i')
 
-
+print('loading page')
 driver.get("https://www.lkqpickyourpart.com/inventory/orlando-1134/")
+
+print('scrolling down')
+scroll_down()
+print('done scrolling')
 
 cars={}
 if os.path.exists('cars.json'):
@@ -46,17 +64,28 @@ cars_e = driver.find_elements(By.XPATH, "//div[@class='pypvi_resultRow']")
 cars = []
 
 for car_e in tqdm(cars_e):
-    car = {}
-    r = re.compile("(\d{4}) (\w*) ([\w ]*).*\nColor")
-    car['year'] = r.search(car_e.text).group(1)
-    car['make'] = r.search(car_e.text).group(2)
-    car['model'] = r.search(car_e.text).group(3)
-    car['vin'] = re.search("VIN: (\w*)", car_e.text).group(1)
-    car['color'] = re.search("Color: (\w*)", car_e.text).group(1)
-    car['section'] = re.search("Section: ([\w\/]*)", car_e.text).group(1)
-    car['row'] = re.search("Row: (\w*)", car_e.text).group(1)
-    car['vin'] = re.search("Space: (\w*)", car_e.text).group(1)
-    car['vin'] = re.search("Stock #: (\w* \w)", car_e.text).group(1)
-    car['vin'] = re.search("Available: (\w*)", car_e.text).group(1)
-    print(car['vin'])
+    try:
+        print(car_e.text)
+        car = {}
+        r = re.compile("(\d{4}) ([\w\-]*) ([\w\- ]*).*\nColor")
+        car['year'] = r.search(car_e.text).group(1)
+        car['make'] = r.search(car_e.text).group(2)
+        car['model'] = r.search(car_e.text).group(3)
+        car['vin'] = re.search("VIN: (\w*)", car_e.text).group(1)
+        car['color'] = re.search("Color: (\w*)", car_e.text).group(1)
+        car['section'] = re.search("Section: ([\w\/]*)", car_e.text).group(1)
+        car['row'] = re.search("Row: (\w*)", car_e.text).group(1)
+        car['space'] = re.search("Space: (\w*)", car_e.text).group(1)
+        car['stock #'] = re.search("Stock #: (\w*.\w)", car_e.text).group(1)
+        car['available'] = re.search("Available: (\w*)", car_e.text).group(1)
+        car['image_url'] = car_e.find_element(By.XPATH, "./a[1]/img[1]").get_attribute("src")
+    except Error:
+        print(Error)
+#    print(f"{car['year']} {car['make']} {car['model']} {car['vin']} {car['image_url']}")
+
+print(f'found {len(cars)} cars')
+
+with open('colors.json', 'w') as f:
+    f.write(json.dumps(cars, indent=4))
+    f.close()
 
